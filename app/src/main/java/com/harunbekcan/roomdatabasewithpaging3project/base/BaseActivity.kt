700b439adcf26f9fb2907af30217c04265b202a9
Harun Bekcan <harun.bekcan@applogist.com>
2022-05-15 15:11:51 +0300

    baseActivity and baseFragment created

---
+++ app/src/main/java/com/harunbekcan/roomdatabasewithpaging3project/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.harunbekcan.roomdatabasewithpaging3project.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VDB : ViewDataBinding> : AppCompatActivity() {
+
+    lateinit var binding: VDB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, getLayoutId())
+    }
+
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+}
---
+++ app/src/main/java/com/harunbekcan/roomdatabasewithpaging3project/base/BaseFragment.kt
@@ -0,0 +1,27 @@
+package com.harunbekcan.roomdatabasewithpaging3project.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VDB : ViewDataBinding> : Fragment() {
+
+    lateinit var binding: VDB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, getLayoutId(), container, false)
+        return binding.root
+    }
+
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+}
